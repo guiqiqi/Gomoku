229526fadac0524663c95db39ffba83cfb26f9ea
Doge <guiqiqi187@gmail.com>
2020-10-18 01:07:37 +0800

    Seprate Game settings and Error definition

---
+++ settings.py
@@ -0,0 +1,45 @@
+"""
+Game settings and Error definition
+"""
+
+
+BLACK = True  # Black Player
+WHITE = False  # White Player
+
+
+class ViewSettings:
+    """Game View settings"""
+    # White space between the edge of the
+    # window and the main interface of the game
+    PADDING = 30
+    ICONMAP = "logo.ico"
+    TITLE = "Gomoku"  # Game window title
+
+    BLACK = "Black"  # Black player
+    WHITE = "White"  # White player
+    BWIN = "#B52016"  # Win color black
+    WWIN = "#EB9B96"  # Win color white
+
+    BGCOLOR = "#FEBE0B"  # Background color for game board
+    BHINT = "#333333"  # Black hinter
+    WHINT = "#F0F0F0"  # White hinter
+
+
+class GameError(Exception):
+    """General game error"""
+
+
+class GameSettingError(Exception):
+    """Error occured during initialization"""
+
+
+class InvalidGridError(GameError):
+    """Invalid piece index"""
+
+
+class SettedGridError(GameError):
+    """Cannot set Grid has been set"""
+
+
+class GameEndedError(GameError):
+    """Raise when game has already ended"""
